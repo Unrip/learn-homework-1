dd3d3a050e24cba5996bc655a5dcd24692c6bc35
unrip1 <unrip1@gmail.com>
2020-09-13 20:49:48 +0300

    решение задания про цикл for

--- 3_for.py
+++ 3_for.py
@@ -10,12 +10,29 @@
 * Посчитать и вывести средний балл по каждому классу.
 """
 
-def main():
+
+def main(classes_scores):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    students_num = 0
+    school_scores_sum = 0
+    for cls in classes_scores:
+        scores_sum = 0
+        for score in cls['scores']:
+            scores_sum += score
+            school_scores_sum += score
+            students_num += 1
+        scores_class_avg = scores_sum / len(cls['scores'])
+        print(f"Средняя оценка {cls['school_class']} класса - {scores_class_avg}")
+    school_scores_avg = school_scores_sum / students_num
+    print(f"Средняя оценка по школе - {school_scores_avg}")
+
+
 if __name__ == "__main__":
-    main()
+    classes_scores = [{'school_class': '4a', 'scores': [3, 4, 4, 5, 2]},
+                      {'school_class': '5b', 'scores': [5, 5, 4, 5, 4, 3, 5]},
+                      {'school_class': '7a', 'scores': [3, 5, 3, 5, 3, 5]},
+                      {'school_class': '9v', 'scores': [2, 4, 3, 2]}]
+    main(classes_scores)
